f80df8037f192a8ee6e0b027a568d9401afae33a
codeck1 <marc.mensajes@gmail.com>
2017-01-27 16:30:25 +0100

    The player can climb the rope

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -29,8 +29,8 @@ public class PlayerBehavior : MonoBehaviour {
 		if (collision.gameObject.CompareTag("Rope"))
 		{
 			Vector3 distance = new Vector3((collision.rigidbody.transform.position.x - transform.position.x), (collision.rigidbody.transform.position.y - transform.position.y), (collision.rigidbody.transform.position.z - transform.position.z));
-			Vector3 newPos = new Vector3(transform.position.x+distance.x, transform.position.y, transform.position.z + distance.z - transform.localScale.z/2);
-			transform.position = newPos;
+			Vector3 newPos = new Vector3(distance.x- transform.localScale.x / 2, 0, distance.z - transform.localScale.z/2);
+			transform.position += newPos;
 			rope = true;
 
 		}

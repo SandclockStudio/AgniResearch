5648caa62a1a6b13033cf577e7c836e538297e29
Vera Rojman <vrojman@gmail.com>
2017-02-06 03:18:37 +0100

    Improve camera rotation trigger script

--- Assets/Scripts/TriggerCameraRotation.cs
+++ Assets/Scripts/TriggerCameraRotation.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class TriggerCameraRotation : MonoBehaviour {
 
+    public float m_RotationTime;
+    public float m_Rotation;
+    private float m_CurrentRotation = 0;
+    private float step;
+
     void OnTriggerEnter (Collider collider) {
     	if (collider.CompareTag("Player")) {
-			Camera.main.transform.Rotate(0, -90, 0);
-    	    collider.transform.Rotate(0, -90, 0);
-	        Destroy(gameObject);
-    	}
+            StartCoroutine(RotateCoroutine(collider));
+        }
+    }
+
+    IEnumerator RotateCoroutine (Collider collider) {
+        while (m_CurrentRotation < Mathf.Abs(m_Rotation)) {
+            step = m_Rotation / m_RotationTime * Time.fixedDeltaTime;
+
+            Camera.main.transform.Rotate(0, step, 0);
+
+            m_CurrentRotation += Mathf.Abs(step);
+            yield return new WaitForSeconds(Time.fixedDeltaTime);
+        }
+
+        collider.transform.Rotate(0, m_Rotation, 0);
+        yield return null;
     }
 }
